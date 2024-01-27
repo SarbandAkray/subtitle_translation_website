b5e4c45561260c559e27317c023149959d7978e6
Sarband <sarbandmuhamad@gmail.com>
2024-01-27 23:40:44 +0300

    changed the fetch function

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,27 +1,19 @@
 export async function translate(line: string) {
-  console.log("origin changed");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "text/plain");
+
+  const raw = line;
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
   const result = await fetch(
     "https://translator-api.glosbe.com/translateByLangWithScore?sourceLang=en&targetLang=ckb",
-    {
-      headers: {
-        accept: "*/*",
-        "accept-language": "en-US,en;q=0.6",
-        "content-type": "text/plain;charset=UTF-8",
-        "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Brave";v="120"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site",
-        "sec-gpc": "1",
-        Referer: "https://glosbe.com/",
-        Origin: "http://localhost",
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        // "Referrer-Policy": "strict-origin-when-cross-origin",
-      },
-      body: line,
-      method: "POST",
-    }
+    requestOptions
   );
 
   return await result.json();
